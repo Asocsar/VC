55d6b5c2b08c20cb0644f558c02dd141e65b4702
Daniel Cano Carrascosa <44205390+Asocsar@users.noreply.github.com>
2021-04-28 18:24:27 +0200

    Plantilla

--- La_Calma_Antes_De_La_Tormenta.m
+++ La_Calma_Antes_De_La_Tormenta.m
@@ -1,5 +1,18 @@
 clear
 
-fileID = fopen('BarzaCamisetaz.txt','r');
-formatSpec = '%f';
-A = fscanf(fileID,formatSpec);
+fid = fopen('Barza_Camisetaz.txt');
+paths = []
+tline = fgetl(fid);
+paths = [paths; tline]
+while ischar(tline)
+    tline = fgetl(fid);
+    if ischar(tline)
+        paths = [paths; tline]
+    end
+end
+fclose(fid);
+
+
+
+IM = imread(paths(1, :));
+imshow(IM);
