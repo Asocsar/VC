f06e1cc4f4a4b6ca4eec647ca7e5058be2b45183
Daniel Cano Carrascosa <44205390+Asocsar@users.noreply.github.com>
2021-05-17 21:23:21 +0200

    Casi hecho wey

---
+++ hist_similarity.m
@@ -0,0 +1,16 @@
+function R = hist_similarity(Means,H)
+[sizeH, elements] = size(H);
+for i = 1:3:elements
+    if R
+end
+R = 1;
+end
+
+
+%dist1 = chi2_distance(R, Means{1});
+%dist2 = chi2_distance(G, Means{2});
+%dist3 = chi2_distance(B, Means{3});
+%p = normcdf(dist1, Mean_R, Std_R);
+%dist = sum((hist1 - hist2).^2 ./ (hist1 + hist2));
+%p = pdist2(R', Means{1}');
+
