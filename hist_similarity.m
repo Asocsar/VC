42f03423b981eb11e8bb1a9511b3965c335d7ffd
Marc Cervilla <marccervi@gmail.com>
2021-05-18 01:09:03 +0200

    Update hist_similarity

--- hist_similarity.m
+++ hist_similarity.m
@@ -1,9 +1,27 @@
-function R = hist_similarity(Means,H)
+function Res = hist_similarity(Means,H)
+threshold = 80;
+
 [sizeH, elements] = size(H);
+
+Res = 0;
+
 for i = 1:3:elements
-    if R
+    if Res==0
+        R = H(:,i);
+        G = H(:,i+1);
+        B = H(:,i+2);
+        
+        dist1 = chi2_distance(R, Means{1});
+        dist2 = chi2_distance(G, Means{2});
+        dist3 = chi2_distance(B, Means{3});
+        mean_dist = (dist1+dist2+dist3)/3;
+        
+        if mean_dist < threshold
+            Res=1;
+        end
+    end
 end
-R = 1;
+%Res = 1;
 end
 
 
