4d506d5d75b352aba8748c96aafc186459587e1f
alvaromacias <56478851+alvaromacias@users.noreply.github.com>
2021-05-21 19:25:44 +0200

    compara con todas las modelo

--- hist_similarity.m
+++ hist_similarity.m
@@ -1,7 +1,8 @@
-function Res = hist_similarity(Means,H)
+function Res = hist_similarity(Hists,H)
 threshold = 50;
 
 [sizeH, elements] = size(H);
+[n, m] = size(Hists);
 
 Res = 0;
 
@@ -12,17 +13,17 @@ for i = 1:3:elements
         B = H(:,i+2);
         Image = cat(3, R, G, B);
         imshow(Image);
-        
-        dist1 = chi2_distance(R, Means{1});
-        dist2 = chi2_distance(G, Means{2});
-        dist3 = chi2_distance(B, Means{3});
-        mean_dist = (dist1+dist2+dist3)/3;
-        
-        if Res == 0
-            Res = mean_dist;
-        
-        else mean_dist < Res
-            Res=mean_dist;
+        for j = 1:3:m
+            dist1 = chi2_distance(R, Hists{j}(:,1));
+            dist2 = chi2_distance(G, Hists{j+1}(:,1));
+            dist3 = chi2_distance(B, Hists{j+2}(:,1));
+            mean_dist = (dist1+dist2+dist3)/3;
+            if Res == 0
+                Res = mean_dist;
+
+            else mean_dist < Res
+                Res=mean_dist;
+            end
         end
     end
 end
