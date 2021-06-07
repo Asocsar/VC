e52d884984a2867286bd6afd499c7adaa5ec3922
Daniel Cano Carrascosa <44205390+Asocsar@users.noreply.github.com>
2021-06-07 22:02:24 +0200

    push dani

--- KNN_HSV.m
+++ KNN_HSV.m
@@ -55,10 +55,11 @@ index_psv = 1;
 
 for i = 1:num_paths
     A = imread(paths{i});
+    A = rgb2hsv(A);
     %imshow(A);
     %C = getrect;
     %C = A(C(2):C(2)+C(4), C(1):C(1)+C(3), :);
-    crops{i} = normColors(A);
+    %crops{i} = normColors(A);
     %A = normColors(A);
     [nC, mC] = size(A(:,:,1));
     meanN = meanN + nC;
@@ -107,38 +108,8 @@ for i = 1:num_paths
     
 end
 
-%Labels_barcelona = nonzeros(int8(Labels == 1));
-%Labels_chelsea = nonzeros(int8(Labels == 2));
-%Labels_juventus = nonzeros(int8(Labels == 3));
-%Labels_liverpool = nonzeros(int8(Labels == 4));
-%Labels_madrid = nonzeros(int8(Labels == 5));
-%Labels_milan = nonzeros(int8(Labels == 6));
-%Labels_psv = nonzeros(int8(Labels == 7));
 
-Labels_barcelona = int8(Labels == 1);
-Labels_chelsea = int8(Labels == 2);
-Labels_juventus = int8(Labels == 3);
-Labels_liverpool = int8(Labels == 4);
-Labels_madrid = int8(Labels == 5);
-Labels_milan = int8(Labels == 6);
-Labels_psv = int8(Labels == 7);
-
-
-%Mdl_barcelona = fitcsvm(Hists_barcelona,Labels_barcelona, 'KernelFunction', 'rbf');
-%Mdl_chelsea = fitcsvm(Hists_chelsea,Labels_chelsea, 'KernelFunction', 'rbf');
-%Mdl_juventus = fitcsvm(Hists_juventus,Labels_juventus, 'KernelFunction', 'rbf');
-%Mdl_liverpool = fitcsvm(Hists_liverpool,Labels_liverpool, 'KernelFunction', 'rbf');
-%Mdl_madrid = fitcsvm(Hists_madrid,Labels_madrid, 'KernelFunction', 'rbf');
-%Mdl_milan = fitcsvm(Hists_milan,Labels_milan, 'KernelFunction', 'rbf');
-%Mdl_psv = fitcsvm(Hists_psv,Labels_psv, 'KernelFunction', 'rbf');
-
-Mdl_barcelona = fitcsvm(Hists,Labels_barcelona, 'Standardize', true);
-Mdl_chelsea = fitcsvm(Hists,Labels_chelsea, 'Standardize', true);
-Mdl_juventus = fitcsvm(Hists,Labels_juventus, 'Standardize', true);
-Mdl_liverpool = fitcsvm(Hists,Labels_liverpool, 'Standardize', true);
-Mdl_madrid = fitcsvm(Hists,Labels_madrid, 'Standardize', true);
-Mdl_milan = fitcsvm(Hists,Labels_milan, 'Standardize', true);
-Mdl_psv = fitcsvm(Hists,Labels_psv, 'Standardize', true);
+Mdl = fitcknn(Hists,Labels, 'NumNeighbors', 3);
 
 
 
@@ -196,13 +167,15 @@ for i = 1:num_paths
     score_madrid = 0;
     score_milan = 0;
     score_psv = 0;
-    for k1 = (cs*6):cs:c-(cs*6)
-        for k2 = (rs*6):rs:r-(rs*6)
+    resta = 5;
+    for k1 = (cs*resta):cs:c-(cs*resta)
+        for k2 = (rs*resta):rs:r-(rs*resta)
             c_end = min(k1+cs, c);
             r_end = min(k2+rs, r);
             k1_start = max(k1, 1);
             k2_start = max(k2, 1);
             M = I(k2_start:r_end, k1_start:c_end, :);
+            M = rgb2hsv(M);
             %imshow(M);
             %M = normColors(M);
             [R, bins] = imhist(M(:,:,1), NBINS);
@@ -210,76 +183,39 @@ for i = 1:num_paths
             [B, bins] = imhist(M(:,:,3), NBINS);
             X = HistNorm([R' G' B']')';
             
-            [label_barcelona,score_svm_barcelona] = predict(Mdl_barcelona,X);
-            [label_chelsea,score_svm_chelsea] = predict(Mdl_chelsea,X);
-            [label_juventus,score_svm_juventus] = predict(Mdl_juventus,X);
-            [label_liverpool,score_svm_liverpool] = predict(Mdl_liverpool,X);
-            [label_madrid,score_svm_madrid] = predict(Mdl_madrid,X);
-            [label_milan,score_svm_milan] = predict(Mdl_milan,X);
-            [label_psv,score_svm_psv] = predict(Mdl_psv,X);
+            [label,score_aux,cost] = predict(Mdl,X);
             
             frontera = 0;
             
-            %if score_svm_barcelona > frontera
-            %    score_barcelona = score_barcelona + 1;
-            %end
-            %if score_svm_chelsea > frontera
-            %    score_chelsea = score_chelsea + 1;
-            %end
-            %if score_svm_juventus > frontera
-            %    score_juventus = score_juventus + 1;
-            %end
-            %if score_svm_liverpool > frontera
-            %    score_liverpool = score_liverpool + 1;
-            %end
-            %if score_svm_madrid > frontera
-            %    score_madrid = score_madrid + 1;
-            %end
-            %if score_svm_milan > frontera
-            %    score_milan = score_milan + 1;
-            %end
-            %if score_svm_psv > frontera
-            %    score_psv = score_psv + 1;
-            %end
-            
-            if label_barcelona > 0
+            if label == 1
                 score_barcelona = score_barcelona + 1;
             end
-            if label_chelsea > 0
+            if label == 2
                 score_chelsea = score_chelsea + 1;
             end
-            if label_juventus > 0
+            if label == 3
                 score_juventus = score_juventus + 1;
             end
-            if label_liverpool > 0
+            if label == 4
                 score_liverpool = score_liverpool + 1;
             end
-            if label_madrid > 0
+            if label == 5
                 score_madrid = score_madrid + 1;
             end
-            if label_milan > 0
+            if label == 6
                 score_milan = score_milan + 1;
             end
-            if label_psv > 0
+            if label == 7
                 score_psv = score_psv + 1;
             end
         end
     end
     
     all_scores = [score_barcelona score_chelsea score_juventus score_liverpool score_madrid score_milan score_psv];
-    %all_scores = mod(all_scores, 20);
     [M, I] = max(all_scores);
     score = all_scores(I);
     
-
-    %disp(paths{i});
-    %disp(["Image is from " Label_teams{I}]);
-        
-    if contains(paths{i}, Label_teams{I})
-        correct = correct + 1;
-    end
-    
-        Y_pred = [Y_pred I];
+    Y_pred = [Y_pred I];
     
     if contains(paths{i}, 'barcelona')
         Y_true = [Y_true 1];
@@ -295,6 +231,11 @@ for i = 1:num_paths
         Y_true = [Y_true 6];
     elseif contains(paths{i}, 'psv')
         Y_true = [Y_true 7];
+    end
+    
+        
+    if contains(paths{i}, Label_teams{I})
+        correct = correct + 1;
     end
     
 end
